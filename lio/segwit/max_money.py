9ff2ad74e071dc35306bb44b3bf56c2acfadd210
Joel Tchoupe <73880183+jojocam666@users.noreply.github.com>
2021-10-16 11:25:00 +0200

    Update max_money.py

--- lio/segwit/max_money.py
+++ lio/segwit/max_money.py
@@ -1,5 +1,12 @@
 
 # Original block reward for miners is 1000 LIO = 50 0000 0000 lilio's
+# Lio mining follows a predefined number of rules making the process very similar to the one in the traditionnal Centrals Banks
+# Total amount of lios is 5 billion distributed in full to the miners-relayers who can introduce the currency in 4 ways on the blockchain:
+#-or by putting it up for sale on the cryptx
+#- or by storing it in the "bpsc-loan the wallet" where it will be distributed in the form of various credits and loans
+#-or by using it for payments
+#-or by blocking a certain amount in the form of a term deposit
+
 start_block_reward = 50 * 10**8
 # 420000 is around every 4 years with a 5 minute block interval
 reward_interval = 420000
